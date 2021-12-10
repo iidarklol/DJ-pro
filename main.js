2b47103f6027f8b3cc56f5912f23a022dc01f378
iidarklol <77841200+iidarklol@users.noreply.github.com>
2021-12-10 12:07:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+Peter_pan_song="";
+Harry_potter_theme_song="";
+
+function setup(){
+    canvas = createCanvas(600,530);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function preload(){
+    music1 = loadSound("music.mp3");
+}
+
+function draw(){
+    image(video,0,0,600,530);}  
